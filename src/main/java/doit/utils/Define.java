c051d550ec3aa8176f4d466f3b78d7de32ba8658
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-01 16:30:58 +0900

    Feat : add constants

--- src/main/java/doit/utils/Define.java
+++ src/main/java/doit/utils/Define.java
@@ -1,5 +1,12 @@
 package doit.utils;
 
 public class Define {
-	public static final int AB_TYPE = 1;	// 임시 설정
+
+	public static final int AB_TYPE = 0;
+	public static final int SAB_TYPE = 1;
+
+	public static final int KOREAN = 1001;
+	public static final int MATH = 2001;
+	public static final int DANCE = 3001;
+
 }
