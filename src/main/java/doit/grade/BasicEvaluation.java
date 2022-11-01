e0a9f0849cfbc6b071b5e7ac29432b1c7bdb4d4c
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-01 16:31:34 +0900

    Refactor : line edit

--- src/main/java/doit/grade/BasicEvaluation.java
+++ src/main/java/doit/grade/BasicEvaluation.java
@@ -1,17 +1,18 @@
 package doit.grade;
 
-public class BasicEvaluation implements GradeEvaluation{
+public class BasicEvaluation implements GradeEvaluation {
 
-	public String getGrade(int point){
+	@Override
+	public String getGrade(int point) {
 		String grade;
 
-		if (point >= 90 && point <= 100){
+		if (point >= 90 && point <= 100) {
 			grade = "A";
-		} else if (point >= 80){
+		} else if (point >= 80) {
 			grade = "B";
-		} else if (point >= 70){
+		} else if (point >= 70) {
 			grade = "C";
-		} else if (point >= 55){
+		} else if (point >= 55) {
 			grade = "D";
 		} else {
 			grade = "F";
